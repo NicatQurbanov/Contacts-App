d9ea2901844fa463244a50909045f28bb5d6e638
Nicat <nicatqur1307@gmail.com>
2026-01-13 10:34:19 +0400

    finsihed

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -11,23 +11,51 @@ namespace Contact
         public static void Main(string[] args)
         {
             string? readResult;
-            string menuSelection;
+            string menuSelection = "";
             ProfileController profileController = new ProfileController();
-            Profile nijat = new Profile("nijat", "qwerty90");
-            Profile ali = new Profile("ali", "qwerty901");
-            Profile vusal = new Profile("vusal", "qwerty902");
-            Profile seyyad = new Profile("seyyad", "qwerty903");
+            //Profile nijat = new Profile("nijat", "qwerty90");
+            //Profile ali = new Profile("ali", "qwerty901");
+            //Profile vusal = new Profile("vusal", "qwerty902");
+            //Profile seyyad = new Profile("seyyad", "qwerty903");
 
-            Contact new_contact = new Contact("nijat", "qurbanov", "+994504949494");
+            //Contact new_contact = new Contact("nijat", "qurbanov", "+994504949494");
 
-            Console.WriteLine("Welcome. Enter your nickname and password to sign in separated by comma(,)");
-            readResult = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Welcome. Choose option:\n1.Sign in.\n2.Sign up.\n");
+                Console.WriteLine("Enter exit to leave application.");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                   menuSelection = readResult.ToLower();
+                
+                switch(menuSelection)
+                {
+                    case "1":
+
+                        Console.WriteLine("Create nickname and password separated by comma (,)\nRequirements: \n" +
+                                        "1. The nickname should consist of at least 3 characters and at most 20 characters.\n" +
+                                        "2. The password should contain at least one uppercase letter, one lowercase letter, one symbol, and one number.\n" +
+                                        "\tThe minimum length of password is: 6\n" +
+                                        "\tThe maximum length of password is: 20\n");
+                        readResult = Console.ReadLine();
+                        
+                        break;
+                    case "2":
+
+
+                        break;
+                }
+
+            } while (menuSelection != "exit");
+            
+
+            
 
             
 
 
-            ContactController contactController = new ContactController(profileController.profiles[0]);
-            contactController.AddContact(new_contact);
+            //ContactController contactController = new ContactController(profileController.profiles[0]);
+            //contactController.AddContact(new_contact);
 
             
 
